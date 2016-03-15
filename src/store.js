3a3dabde53b24338d28d1cfcee4322ad8702b549
Justin <juns3700@gmail.com>
2016-03-15 21:38:25 +0800

    use thunkMiddleware

--- src/store.js
+++ src/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import msgApp from './reducers';
 
 export default (initialState) => {
-  const store = compose(
-      applyMiddleware(
-          thunk
-      ),
-      window.devToolsExtension ? window.devToolsExtension() : f => f
-  )(createStore)(msgApp, initialState);
+  	const store = compose(
+      	applyMiddleware(
+          	thunkMiddleware
+      	),
+      	window.devToolsExtension ? window.devToolsExtension() : f => f
+  	)(createStore)(msgApp, initialState);
 
-  return store;
+  	return store;
 };
